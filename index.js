c3ba6b06351d543f370e020a4dcc591dfc71fc13
BerthumNorud <berthum.ondur@gmail.com>
2023-03-01 14:50:12 -0600

    var vs let vs const

---
+++ index.js
@@ -0,0 +1,11 @@
+/**
+ * var vs let vs const 
+ * 
+ * var allows you to declare 2 variables with the same name and change the value
+ * let don't allows you to declare 2 variables with the same name
+ */
+
+var example = 20
+var example = 25
+console.log(example)
+
