6a6848038af68564437a0318578f108f64efed20
Solrac Norud <carlos.duron@cceo.com.mx>
2023-03-02 10:13:44 -0600

    Pintar Objeto con sus propiedades e index

--- mi-primer-vue/src/App.vue
+++ mi-primer-vue/src/App.vue
@@ -1,30 +1,18 @@
 <script setup>
 const name = 'Vue Dinamico'
-const arrayFrutas = [
-        {
-            name: "Manzana",
-            price: "$1.00",
-            description: "Una manzana",
-        },
-        {
-            name: "Pera",
-            price: "$2.00",
-            description: "Una pera",
-        },
-        {
-            name: "Naranja",
-            price: "$3.00",
-            description: "Una naranja",
-        },
-    ];
+const objetoFruta = {
+  name: "Manzana",
+  price: "$1.00",
+  description: "Una manzana",
+  id:1
+}
 </script>
 
 <template>
   <h1> Hola {{ name.toUpperCase() }}</h1>
   <ul>
-    <li v-for="(fruta, index) in arrayFrutas" 
-      :key="fruta.name">
-      {{ index + 1 }} - {{ fruta.name }}  {{ fruta.price }}   {{ fruta.description }} 
+    <li v-for="(value, prop, index) in objetoFruta" :key="index">
+      {{ index }} - {{ prop }} : {{ value }}
     </li>
   </ul>
 </template>
