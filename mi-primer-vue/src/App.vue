67d8fe4bbc860d8d3df7d4601f091d0cbedb5025
Solrac Norud <carlos.duron@cceo.com.mx>
2023-03-02 10:39:30 -0600

    Eventos 1

--- mi-primer-vue/src/App.vue
+++ mi-primer-vue/src/App.vue
@@ -1,44 +1,19 @@
-<template>
-  <div>
-    <h2>Lista de usuarios:</h2>
-    <ul>
-      <template
-        v-for="(user, index) in users"
-        :key="user.id"
-      >
-        <li>
-          <h3>{{ user.name }}</h3>
-          <ul>
-            <template
-              v-for="(post, index) in user.posts"
-              :key="post.id"
-            >
-              <li>{{ post.title }}</li>
-            </template>
-          </ul>
-        </li>
-      </template>
-    </ul>
-</div>
-</template>
- 
 <script setup>
-const users = [
-  {
-    id: 1,
-    name: 'Juan',
-    posts: [
-      { id: 1, title: 'Mi primer post' },
-      { id: 2, title: 'Mi segundo post' },
-    ]
-  },
-  {
-    id: 2,
-    name: 'Maria',
-    posts: [
-      { id: 3, title: 'Mi tercer post' },
-      { id: 4, title: 'Mi cuarto post' },
-    ]
-  }
-]
+const saludo = 'desconocido!'
+
+//metodo - methods
+const handleClick = message => console.log(message)
 </script>
+
+<template>
+  <h1> ¡Hola {{ saludo }}</h1>
+  <button v-on:click="handleClick('¡Me activaste 1!')">Activame 1</button>
+  <br>
+  <button @:click="handleClick('¡Me activaste 2!')">Activame 2</button>
+</template>
+
+<style>
+h1 {
+  color: aqua;
+}
+</style>
