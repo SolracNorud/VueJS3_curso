fb3d0f52bd268bf2a330749d6fd17b782d32238d
Solrac Norud <carlos.duron@cceo.com.mx>
2023-03-02 12:31:09 -0600

    Practica 2

--- mi-primer-vue/src/App.vue
+++ mi-primer-vue/src/App.vue
@@ -1,7 +1,8 @@
 <script setup>
-import {ref, computed} from 'vue'
+import { ref, computed } from 'vue'
 
 const counter = ref(0)
+const arrayFav = ref([])
 
 //metodo - methods
 const increment = () => counter.value++
@@ -9,19 +10,32 @@ const decrement = () => counter.value--
 const reset = () => counter.value = 0
 
 const classCounter = computed(() => {
-    if (counter.value === 0) {
-        return "zero";
-    }
-    return counter.value > 0 ? "positive" : "negative";
+  if (counter.value === 0) {
+    return "zero";
+  }
+  return counter.value > 0 ? "positive" : "negative";
 });
+
+const add = () => arrayFav.value.push(counter.value)
+
+const bloquearBtnAdd = computed(() => {
+  const numSearch = arrayFav.value.find(num => num === counter.value)
+  // if (numSearch === 0) return true
+  // return numSearch ? true : false
+  return numSearch || numSearch === 0
+})
 </script>
 
 <template>
-  <h1> ¡Computed! </h1>
+  <h1> ¡Práctica 2! </h1>
   <h2 :class="classCounter">{{ counter }}</h2>
   <button @click="increment()">Incrementar</button>
   <button @click="decrement()">Decrementar</button>
   <button @click="reset()">Restablecer</button>
+  <button @click="add(counter)" :disabled="bloquearBtnAdd">Favorito</button>
+  <ul>
+    <li v-for="(num, index) in arrayFav" :key="index"> {{ num }}</li>
+  </ul>
 
 </template>
 
@@ -29,13 +43,15 @@ const classCounter = computed(() => {
 h1 {
   color: aqua;
 }
+
 .positive {
   color: green;
 }
+
 .negative {
   color: red;
 }
-.zero{
+
+.zero {
   color: white;
-}
-</style>
+}</style>
