3622ac8a286c1b82cbd97ebbc0708fdbf378b7c8
Solrac Norud <carlos.duron@cceo.com.mx>
2023-03-02 09:58:10 -0600

    For en con un array (Frutas 1)

--- mi-primer-vue/src/App.vue
+++ mi-primer-vue/src/App.vue
@@ -1,28 +1,17 @@
 <script setup>
 const name = 'Vue Dinamico'
-const styleColor = "color: white"
-const arrayColores = ["green", "white", "orange"]
-const active = null
+const arrayFrutas = ["🍎", "🍌", "🍉", "🍓", "🍒"];
 </script>
 
 <template>
   <h1> Hola {{ name.toUpperCase() }}</h1>
-  <br>
-  <h2> {{ arrayColores }}</h2>
-  <br>
-  <h2 :style="`color:${arrayColores[2]}`"> Soy Naranja </h2>
-  <br>
-  <h2>
-    {{ active ? 'Estoy activo' : 'Estoy inactivo' }}
-  </h2>
-  <br>
-  <p v-if= "active === true">Estoy activo</p>
-  <p v-else-if ="active === false">Estoy inactivo</p>
-  <p v-else>Esto indeciso</p>
+  <ul>
+    <li v-for="(fruta, index) in arrayFrutas" :key="index">{{ index + 1 }} - {{ fruta }} </li>
+  </ul>
 </template>
 
 <style>
-h1{
-  color:aqua;
+h1 {
+  color: aqua;
 }
 </style>
