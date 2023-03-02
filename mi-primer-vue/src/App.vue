4d706520c66f5b737ad6a107051633960226d9d1
Solrac Norud <carlos.duron@cceo.com.mx>
2023-03-02 11:26:48 -0600

    Práctica desafio

--- mi-primer-vue/src/App.vue
+++ mi-primer-vue/src/App.vue
@@ -1,23 +1,35 @@
 <script setup>
 import {ref} from 'vue'
 
-const saludo = 'desconocido!'
-
 const counter = ref(0)
 
 //metodo - methods
 const increment = () => counter.value++
+const decrement = () => counter.value--
+const reset = () => counter.value = 0
 
+if (counter.value < 0){
+  color.value  = ref(1)
+}
 </script>
 
 <template>
-  <h1> ¡Variables reactivas!</h1>
-  <h2>{{ counter }}</h2>
+  <h1> ¡Práctica! </h1>
+  <h2 :class="counter > 0 ? 'positive' : 'negative' ">{{ counter }}</h2>
   <button @click="increment()">Incrementar</button>
+  <button @click="decrement()">Decrementar</button>
+  <button @click="reset()">Restablecer</button>
+
 </template>
 
 <style>
 h1 {
   color: aqua;
+}
+.positive {
+  color: green;
+}
+.negative {
+  color: red;
 }
 </style>
