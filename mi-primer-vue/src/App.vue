5c5dd4658e18d5c4dfb35f9c433919d845286708
Solrac Norud <carlos.duron@cceo.com.mx>
2023-03-02 10:05:47 -0600

    Recorrer array de objetos con For

--- mi-primer-vue/src/App.vue
+++ mi-primer-vue/src/App.vue
@@ -1,12 +1,31 @@
 <script setup>
 const name = 'Vue Dinamico'
-const arrayFrutas = ["🍎", "🍌", "🍉", "🍓", "🍒"];
+const arrayFrutas = [
+        {
+            name: "Manzana",
+            price: "$1.00",
+            description: "Una manzana",
+        },
+        {
+            name: "Pera",
+            price: "$2.00",
+            description: "Una pera",
+        },
+        {
+            name: "Naranja",
+            price: "$3.00",
+            description: "Una naranja",
+        },
+    ];
 </script>
 
 <template>
   <h1> Hola {{ name.toUpperCase() }}</h1>
   <ul>
-    <li v-for="(fruta, index) in arrayFrutas" :key="index">{{ index + 1 }} - {{ fruta }} </li>
+    <li v-for="(fruta, index) in arrayFrutas" 
+      :key="fruta.name">
+      {{ index + 1 }} - {{ fruta.name }}  {{ fruta.price }}   {{ fruta.description }} 
+    </li>
   </ul>
 </template>
 
