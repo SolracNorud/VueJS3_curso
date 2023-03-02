e2ba518bf6af7842df93fbada4284da38aa24f74
Solrac Norud <carlos.duron@cceo.com.mx>
2023-03-02 09:00:42 -0600

    Mi primer vue

--- index.js
+++ index.js
@@ -87,3 +87,31 @@ const web = {
 const {nombre} = web.redesSociales.youtube
 //console.log(nombre)
 
+/**
+ * fetch
+ */
+
+fetch('https://pokeapi.co/api/v2/pokemon')
+    .then(res => res.json())
+    .then(data => {
+        data.results.forEach(element => {
+            //console.log(element)
+        });
+    })
+    .catch(error => console.log(error))
+
+/**
+ * async await
+ */
+
+const obtenerPokemon = async() => {
+    try{
+        const res = await fetch ('https://pokeapi.co/api/v2/pokemon')
+        const data = await res.json()
+        console.log(data.results)
+    } catch(error){
+        console.log(error)
+    }
+}
+
+obtenerPokemon()
