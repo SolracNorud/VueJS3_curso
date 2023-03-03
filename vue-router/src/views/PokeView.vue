ca27f032f4b2912732342be172bf6f04026b7fda
Solrac Norud <carlos.duron@cceo.com.mx>
2023-03-03 11:50:22 -0600

    :sparkles: Sección 6
    Finalizado

--- vue-router/src/views/PokeView.vue
+++ vue-router/src/views/PokeView.vue
@@ -1,44 +1,33 @@
 <script setup>
-import { ref } from "vue";
-import axios from "axios";
 import { useRoute, useRouter } from "vue-router";
+import {} from "@/composables/getData";
+import { useGetData } from "../composables/getData";
 
 const route = useRoute();
 const router = useRouter();
 
+const { getData, data, loading, errorData } = useGetData();
+
 const back = () => {
   router.push("/pokemons");
 };
-const poke = ref({});
 
-const getData = async () => {
-  try {
-    const { data } = await axios.get(
-      `https://pokeapi.co/api/v2/pokemon/${route.params.name}`
-    );
-    console.log(data);
-    poke.value = data;
-  } catch (error) {
-    console.log(error);
-    poke.value = null;
-  }
-};
-
-getData();
+getData(`https://pokeapi.co/api/v2/pokemon/${route.params.name}`);
 </script>
 
 <template>
-  <div v-if="poke">
+  <p v-if="loading">Cargando Información</p>
+  <div class="alert alert-danger mt-2" v-if="errorData">{{ errorData }}</div>
+  <div v-if="data">
     <h1>Pokemon name: {{ $route.params.name }}</h1>
-    <img :src="poke.sprites?.front_default" alt="" />
-    <h2 v-for="ability in poke.abilities">
+    <img :src="data.sprites?.front_default" alt="" />
+    <h2 v-for="ability in data.abilities">
       Ability: {{ ability["ability"]["name"] }}
     </h2>
     <h2>STATS</h2>
-    <h2 v-for="stat in poke.stats">
+    <h2 v-for="stat in data.stats">
       Name: "{{ stat.stat.name }}" --- Base stat: {{ stat.base_stat }}
     </h2>
   </div>
-  <h1 v-else>No existe este pokemon</h1>
   <button class="btn btn-outline-primary" @click="back">Back</button>
 </template>
